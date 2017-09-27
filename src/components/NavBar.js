bb22103c518b5ae22528f89af86fef894b7d4221
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-27 13:29:10 -0400

    working on adding arrays to backend

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,28 +5,21 @@ import { bindActionCreators } from 'redux'
 import { logoutUser } from '../actions/authActions'
 
 class NavBar extends React.Component {
-	state = {
-		activeItem: 'home'
-	}
 
 
-	handleItemClick = (e, { name }) => {
-		this.setState({
-			activeItem: name
-		})
-	}
+
 
 	handleLogOut = (event) => {
 		this.props.logoutUser()
 	}
 
 	render() {
-		const { activeItem } = this.state
+
 
 		return(
 			<Menu inverted>
-				<Menu.Item name='home' href='/profile' onClick={this.handleItemClick} />
-				<Menu.Item name='wineSearch' href='/winesearch' onClick={this.handleItemClick} />
+				<Menu.Item name='home' href='/profile' />
+				<Menu.Item name='wineSearch' href='/winesearch' />
 				<Menu.Menu position='right'>
 					<Menu.Item>
 						<Button primary href='/login'>Log In</Button>
