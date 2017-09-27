0579da3b7489011c61ecc9b5e993736e22a4471f
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-26 23:15:14 -0400

    updated WinePage

--- src/components/wineComponents/WinePage.js
+++ src/components/wineComponents/WinePage.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Card, Image } from 'semantic-ui-react'
 
 class WinePage extends React.Component {
 
 	render() {
 		console.log(this.props)
 		return(
-			<div> I AM THE WINE PAGE </div>
+			<div>
+				<Card className="ui container center aligned">
+					<Image className="ui container center aligned" src={this.props.wine.currentWine.image} size="small"/>
+					<Card.Content>
+						<Card.Header>
+						{this.props.wine.currentWine.name}
+						</Card.Header>
+					</Card.Content>
+				</Card>
+			</div>
 		)
 	}
 
