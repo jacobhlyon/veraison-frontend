0956f35141d02f78fb1906a41f1507984feb651e
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-27 16:54:23 -0400

    added initial Victory chart to render wine info

--- src/components/wineComponents/WinePage.js
+++ src/components/wineComponents/WinePage.js
@@ -1,62 +1,41 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, Image, Table } from 'semantic-ui-react'
+import { Image, Grid } from 'semantic-ui-react'
+import WinePalateRadarChart from './WinePalateRadarChart'
 
 class WinePage extends React.Component {
 
 
 	render() {
 
-		const sightValues = Object.values(this.props.wine.currentSightScore)
-		const sightResults = sightValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
+		// const sightValues = Object.values(this.props.wine.currentSightScore)
+		// const sightResults = sightValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
 		
-		const noseValues = Object.values(this.props.wine.currentNoseScore)
-		const noseResults = noseValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
+		// const noseValues = Object.values(this.props.wine.currentNoseScore)
+		// const noseResults = noseValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
 
-		const palateValues = Object.values(this.props.wine.currentPalateScore)
-		const palateResults = palateValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
+		// const palateValues = Object.values(this.props.wine.currentPalateScore)
+		// const palateResults = palateValues.map((value, index) => {return <Table.Row key={index}><Table.Cell>{value}</Table.Cell></Table.Row>})
 
 		return(
-			<div>
-				<Card className="ui container center aligned">
-					<Image className="ui container center aligned" src={this.props.wine.currentWine.image} size="small"/>
-					<Card.Content>
-						<Card.Header>
-						{this.props.wine.currentWine.name}
-						</Card.Header>
-					</Card.Content>
-				</Card>
-				<Table striped>
-					<Table.Header>
-						<Table.Row>
-							<Table.HeaderCell>Sight Results</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						{sightResults}
-					</Table.Body>
-				</Table>
-				<Table striped>
-					<Table.Header>
-						<Table.Row>
-							<Table.HeaderCell>Nose Results</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						{noseResults}
-					</Table.Body>
-				</Table>
-				<Table striped>
-					<Table.Header>
-						<Table.Row>
-							<Table.HeaderCell>Palate Results</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						{palateResults}
-					</Table.Body>
-				</Table>
-			</div>
+			<Grid celled>
+				<Grid.Row>
+					<Grid.Column width={4}>
+						<Image src={this.props.wine.currentWine.image} />
+					</Grid.Column>
+					<Grid.Column width={8}>
+						<h2>{this.props.wine.currentWine.name}</h2>
+					</Grid.Column>
+				</Grid.Row>
+				<Grid.Row>
+					<Grid.Column width={8}>
+						<h2>I am some material </h2>
+					</Grid.Column>
+					<Grid.Column width={4}>
+						<WinePalateRadarChart />	
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
 		)
 	}
 
