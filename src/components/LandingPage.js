81630cfb56d4ddfe9c1b3838af1b83e88b846f65
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-10-05 15:25:52 -0400

    final push before presentation

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Header, Grid } from 'semantic-ui-react'
 
 const LandingPage = () => {
 	return(
 		<div>
-			<h1>Welcome to Veraison</h1>
-			<h3>Ripen your knowledge</h3>
-			<Button primary href='/signup'>Create an Account</Button>
+		<Grid>
+			<Grid.Row>
+				<Grid.Column width={2}></Grid.Column>
+				<Grid.Column width={6}>
+					<br /><br />
+					<Header as='h1' style={{fontWeight: "bolder", fontSize: "500%"}}>Veraison</Header>
+					<Header as='h3' style={{fontWeight: "lighter"}}>Ripen your knowledge</Header>
+					<br />
+					<Button inverted href='/signup'>Create an Account</Button>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+		<video poster="versaison.jpg" id="bgvid" playsInline autoPlay muted loop>
+        	<source src="/veraison.mp4" type="video/mp4" />
+    	</video>
 		</div>
 	)
 }
