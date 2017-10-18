a791be5c9111c2760e3dd001ce84c900c87ec394
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-10-18 09:32:44 -0400

    fixing snooth bug

--- src/actions/wineActions.js
+++ src/actions/wineActions.js
@@ -1,7 +1,7 @@
 
 export function searchNewWine(winery, varietal, vintage) {
 	return function(dispatch) {
-		return fetch(`//api.snooth.com/wines/?akey=6dq1avtmtsp562d932xzhdhtpdwycu9ivmbus921nkru0jrn&q=${winery}+${varietal}+${vintage}`)
+		return fetch(`https://api.snooth.com/wines/?akey=6dq1avtmtsp562d932xzhdhtpdwycu9ivmbus921nkru0jrn&q=${winery}+${varietal}+${vintage}`)
 			.then(res => res.json())
 			.then(json => {
 				dispatch({type: "WINE_SEARCH_RESULTS", payload: json})
