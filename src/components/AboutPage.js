f0ef7eaf3acb7c129b0cf07501678b010ad19443
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-11-07 20:51:37 -0500

    updating about page CSS

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Segment, Header } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 const AboutPage = () => {
 	return (
-		<Segment>
+		<Container text>
 			<Header as='h2' >What is Veraison?</Header>
 			<p>Veraison is a wine-tasting app designed to help wine professionals keep track of and compare tasting notes.</p>
 			<p>It is based on the deductive tasting grid from the <a href='http://www.courtofmastersommeliers.org/other/deductive-tasting-grid/'>Court of Master Sommeliers</a> to help improve your palate. The idea is that objectively tasting wine looking for the same critera will help increase palate awareness and allow you to distinguish the nuances in different wines.</p>
 			<p>Veraison allows you to search for the specific wine you are tasting, guides you through the tasting process, and then shows your results compared to the average of all other users. This will help you know if you're tasting the same as everyone else!</p>
 			<p>Practice makes perfect :)</p>
-		</Segment>
+		</Container>
 	)
 }
 
