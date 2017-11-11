f99c9eaf83c768ca0c9481f968f53daea2e3fbc4
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-11-11 12:58:23 -0500

    added signup button to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Button } from 'semantic-ui-react'
 
 const AboutPage = () => {
 	return (
@@ -9,6 +9,8 @@ const AboutPage = () => {
 			<p>It is based on the deductive tasting grid from the <a href='http://www.courtofmastersommeliers.org/other/deductive-tasting-grid/'>Court of Master Sommeliers</a> to help improve your palate. The idea is that objectively tasting wine looking for the same critera will help increase palate awareness and allow you to distinguish the nuances in different wines.</p>
 			<p>Veraison allows you to search for the specific wine you are tasting, guides you through the tasting process, and then shows your results compared to the average of all other users. This will help you know if you're tasting the same as everyone else!</p>
 			<p>Practice makes perfect :)</p>
+			<br />
+			<Button inverted href='/signup'>Create an Account</Button>
 		</Container>
 	)
 }
